71712c9e17a62bb5fafa932d668a4d97ed911fa9
Carlos Roberto <carlosjr.if@gmail.com>
2025-09-18 21:00:16 -0300

    feat: add abstract class to define assign methods to send notification channels sms and email

--- src/core/ports/gateways/i_notification_sender_gateway.py
+++ src/core/ports/gateways/i_notification_sender_gateway.py
@@ -1,8 +1,3 @@
-"""
-Essa classe abstrata fornece métodos de envio de mensagem SMS e E-mail.
-A classe que implementa-la poderá definir como o envio será feito.
-Para este projeto a classe que implementará esses métodos utilizará recursos da AWS (SNS e SES).
-"""
 from abc import ABC, abstractmethod
 
 
